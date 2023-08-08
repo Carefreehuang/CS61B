3307486c9e8694494439b6d3e1ead2272bc25abd
Carefreehuang <2380520989@qq.com>
2023-08-08 21:45:38 +0800

    initially submission

--- lab8/hashmap/Map61B.java
+++ lab8/hashmap/Map61B.java
@@ -2,48 +2,43 @@ package hashmap;
 
 import java.util.Set;
 /**
- * Your implementation hashmap.MyHashMap should implement this interface. To do so,
- * append "implements hashmap.Map61B<K, V>" to the end of your "public class..."
- * declaration, though you can use other formal type parameters if you'd like.
+ * 你的 hashmap.MyHashMap 实现应该实现这个接口。为了做到这一点，
+ * 在你的 "public class..." 声明的结尾添加 "implements hashmap.Map61B<K, V>"，
+ * 虽然你也可以使用其他形式的类型参数。
  */
 public interface Map61B<K, V> extends Iterable<K> {
-    /** Removes all of the mappings from this map. */
+    /** 从这个映射中删除所有映射。 */
     void clear();
 
-    /** Returns true if this map contains a mapping for the specified key. */
+    /** 如果此映射包含指定键的映射，则返回 true。 */
     boolean containsKey(K key);
 
     /**
-     * Returns the value to which the specified key is mapped, or null if this
-     * map contains no mapping for the key.
+     * 返回指定键映射到的值，如果此映射不包含该键的映射，则返回 null。
      */
     V get(K key);
 
-    /** Returns the number of key-value mappings in this map. */
+    /** 返回此映射中的键值对数量。 */
     int size();
 
     /**
-     * Associates the specified value with the specified key in this map.
-     * If the map previously contained a mapping for the key,
-     * the old value is replaced.
+     * 将指定值与指定键关联在此映射中。
+     * 如果映射先前包含键的映射，则旧值将被替换。
      */
     void put(K key, V value);
 
-    /** Returns a Set view of the keys contained in this map. */
+    /** 返回此映射中包含的键的 Set 视图。 */
     Set<K> keySet();
 
     /**
-     * Removes the mapping for the specified key from this map if present.
-     * Not required for Lab 8. If you don't implement this, throw an
-     * UnsupportedOperationException.
+     * 如果存在，则从该映射中移除指定键的映射。
+     * 对于实验 8 不是必需的。如果你不实现这个方法，请抛出 UnsupportedOperationException。
      */
     V remove(K key);
 
     /**
-     * Removes the entry for the specified key only if it is currently mapped to
-     * the specified value. Not required for Lab 8. If you don't implement this,
-     * throw an UnsupportedOperationException.
+     * 仅在指定键当前映射到指定值时才删除该键的条目。
+     * 对于实验 8 不是必需的。如果你不实现这个方法，请抛出 UnsupportedOperationException。
      */
     V remove(K key, V value);
 }
-
--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -1,19 +1,17 @@
 package hashmap;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
- *  A hash table-backed Map implementation. Provides amortized constant time
- *  access to elements via get(), remove(), and put() in the best case.
+ * 基于哈希表的映射实现。在最好的情况下，通过 get()、remove() 和 put() 方法提供了平摊常数时间的元素访问。
  *
- *  Assumes null keys will never be inserted, and does not resize down upon remove().
- *  @author YOUR NAME HERE
+ * 假设不会插入空键，并且在删除时不会缩小。author HUANGZIHAO
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
     /**
-     * Protected helper class to store key/value pairs
-     * The protected qualifier allows subclass access
+     * 保护的辅助类，用于存储键/值对
+     * protected 修饰符允许子类访问
      */
     protected class Node {
         K key;
@@ -25,67 +23,170 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
         }
     }
 
-    /* Instance Variables */
+    /* 实例变量 */
+    private static final int  DEFAULT_INITIALSIZE = 16;
+    private static final double DEFAULT_LOADFACTOR = 0.75;
     private Collection<Node>[] buckets;
-    // You should probably define some more!
+    private int initialSize;
+    private double loadFactor;
+    private int bucketSize;
+    private int nodeSize;
+    private Set<K> keys;  // key的集合
+    // 你可能还需要定义一些变量！
 
-    /** Constructors */
-    public MyHashMap() { }
+    /** 构造函数 */
+    public MyHashMap() {
+        this(DEFAULT_INITIALSIZE,DEFAULT_LOADFACTOR);   //调用 MyHashMap(int initialSize, double maxLoad)
+    }
 
-    public MyHashMap(int initialSize) { }
+    public MyHashMap(int initialSize) {
+        this(initialSize,DEFAULT_LOADFACTOR);
+    }
 
     /**
-     * MyHashMap constructor that creates a backing array of initialSize.
-     * The load factor (# items / # buckets) should always be <= loadFactor
+     * MyHashMap 构造函数，创建初始大小的支持数组。
+     * 负载因子（项数 / 桶数）应始终 <= loadFactor。
      *
-     * @param initialSize initial size of backing array
-     * @param maxLoad maximum load factor
+     * @param initialSize 支持数组的初始大小
+     * @param maxLoad 最大负载因子
      */
-    public MyHashMap(int initialSize, double maxLoad) { }
+    public MyHashMap(int initialSize, double maxLoad) {
+        this.initialSize = initialSize;
+        this.loadFactor = maxLoad;
+        this.buckets = createTable(this.initialSize);
+        this.nodeSize = 0;
+        this.bucketSize = initialSize;
+        keys = new HashSet<>();
+    }
 
     /**
-     * Returns a new node to be placed in a hash table bucket
+     * 返回一个新的节点，将放置在哈希表桶中
      */
     private Node createNode(K key, V value) {
-        return null;
+        return new Node(key, value);
     }
 
     /**
-     * Returns a data structure to be a hash table bucket
+     * 返回一个数据结构作为哈希表桶
      *
-     * The only requirements of a hash table bucket are that we can:
-     *  1. Insert items (`add` method)
-     *  2. Remove items (`remove` method)
-     *  3. Iterate through items (`iterator` method)
+     * 哈希表桶的唯一要求是我们可以：
+     *  1. 插入项目（`add` 方法）
+     *  2. 移除项目（`remove` 方法）
+     *  3. 遍历项目（`iterator` 方法）
      *
-     * Each of these methods is supported by java.util.Collection,
-     * Most data structures in Java inherit from Collection, so we
-     * can use almost any data structure as our buckets.
+     * 每个这些方法都由 java.util.Collection 支持，
+     * Java 中的大多数数据结构都继承自 Collection，因此我们可以
+     * 使用几乎任何数据结构作为我们的桶。
      *
-     * Override this method to use different data structures as
-     * the underlying bucket type
+     * 重写此方法以使用不同的数据结构作为底层桶类型
      *
-     * BE SURE TO CALL THIS FACTORY METHOD INSTEAD OF CREATING YOUR
-     * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
+     * 请确保调用此工厂方法，而不是使用 new 运算符创建自己的桶数据结构！
      */
     protected Collection<Node> createBucket() {
-        return null;
+        return new LinkedList<>();
     }
 
     /**
-     * Returns a table to back our hash table. As per the comment
-     * above, this table can be an array of Collection objects
+     * 返回用于支撑哈希表的表格。根据上面的注释，
+     * 这个表格可以是 Collection 对象的数组
      *
-     * BE SURE TO CALL THIS FACTORY METHOD WHEN CREATING A TABLE SO
-     * THAT ALL BUCKET TYPES ARE OF JAVA.UTIL.COLLECTION
+     * 请务必在创建表格时调用此工厂方法，
+     * 以便所有桶类型都是 JAVA.UTIL.COLLECTION
      *
-     * @param tableSize the size of the table to create
+     * @param tableSize 要创建的表格大小
      */
     private Collection<Node>[] createTable(int tableSize) {
-        return null;
+        Collection[] buckets = new Collection[tableSize];
+        for (int i = 0; i < tableSize; i++) {
+            buckets[i] = createBucket();
+        }
+        return buckets;
+    }
+    private int getIndex(K key){
+        return Math.abs(key.hashCode()) % this.bucketSize;
     }
 
-    // TODO: Implement the methods of the Map61B Interface below
-    // Your code won't compile until you do so!
+    // TODO：在下面实现 Map61B 接口的方法
+    // 你的代码在实现这些方法之前将无法编译！
+    @Override
+    public int size() {
+        return this.nodeSize;
+    }
 
+    @Override
+    public boolean containsKey(K key) {
+        if (keys == null){
+            return false;
+        }
+        return keys.contains(key);
+    }
+
+    @Override
+    public V get(K key) {
+        if (!containsKey(key)){
+            return null;
+        }
+            int index = getIndex(key);
+            for (Node node:buckets[index]) {
+                if (node.key.equals(key)){
+                    return node.value;
+                }
+            }
+            return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < this.bucketSize; i++) {
+            buckets[i] = null;
+        }
+        keys =null;
+        nodeSize = 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        int index = getIndex(key);  // 要让映射均匀分布。
+        if (this.buckets[index] != null) {//判断bucket是否为空
+            if (!containsKey(key)) {
+                buckets[index].add(createNode(key, value));
+                this.nodeSize += 1;
+                keys.add(key);
+            } else {
+                for (Node node : buckets[index]) {
+                    if (node.key.equals(key)) {
+                        node.value = value;
+                    }
+                }
+            }
+        }
+        if (!containsKey(key)) {
+            buckets[index].add(createNode(key, value));
+            this.nodeSize += 1;
+            keys.add(key);
+        }
+    }
+
+
+
+    @Override
+    public Set<K> keySet() {
+        return keys;
+    }
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
 }
