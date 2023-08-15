8b156398006246cda54b94d227824538107487cd
Carefreehuang <2380520989@qq.com>
2023-08-15 18:02:53 +0800

    update

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -10,15 +10,13 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         // TODO: what if args is empty?
         if (args.length == 0) {  //如果参数为空
             System.out.println("Please enter a command.");
             System.exit(0);
         }
         String firstArg = args[0];
-
-
             switch (firstArg) {
                 case "init":
                     // TODO: handle the `init` command
