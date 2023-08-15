69d007c518a1458ec22d74224b14da8890b99201
Carefreehuang <2380520989@qq.com>
2023-08-15 17:33:08 +0800

    update

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -17,64 +17,70 @@ public class Main {
             System.exit(0);
         }
         String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                // TODO: handle the `init` command
-                validateNumArgs(args, 1);
-                Repository.init();
-                break;
-            case "add":
-                // TODO: handle the `add [filename]` command
-                validateNumArgs(args,2);
-                Repository.add(args[1]);
-                break;
-            case "commit":
-                validateNumArgs(args,2);
-                Repository.commit(args[1]);
-                break;
-            case "log":
-                validateNumArgs(args,1);
-                Repository.log();
-                break;
-            case "rm":
-                validateNumArgs(args,2);
-                Repository.rm(args[1]);
-                break;
-            case "status":
-                validateNumArgs(args,1);
-                Repository.status();
-                break;
-            case "global-log":
-                validateNumArgs(args,1);
-                Repository.globallog();
-                break;
-            case "find":
-                validateNumArgs(args,2);
-                Repository.find(args[1]);
-                break;
-            case "checkout":
-                Repository.checkout(args);
-                break;
-            case "branch":
-                validateNumArgs(args,2);
-                Repository.branch(args[1]);
-                break;
-            case "rm-branch":
-                validateNumArgs(args,2);
-                Repository.rmbranch(args[1]);
-                break;
-            case "reset":
-                validateNumArgs(args,2);
-                Repository.reset(args[1]);
-                break;
-            case "merge":
-                validateNumArgs(args,2);
-                Repository.merge(args[1]);
-            break;
-            // TODO: FILL THE REST IN
-            default:  //如果改命令不是上面几个命令，输出....并exit；
-                System.out.println("No command with that name exists.");
-                System.exit(0);
+        try {
+
+
+            switch (firstArg) {
+                case "init":
+                    // TODO: handle the `init` command
+                    validateNumArgs(args, 1);
+                    Repository.init();
+                    break;
+                case "add":
+                    // TODO: handle the `add [filename]` command
+                    validateNumArgs(args, 2);
+                    Repository.add(args[1]);
+                    break;
+                case "commit":
+                    validateNumArgs(args, 2);
+                    Repository.commit(args[1]);
+                    break;
+                case "log":
+                    validateNumArgs(args, 1);
+                    Repository.log();
+                    break;
+                case "rm":
+                    validateNumArgs(args, 2);
+                    Repository.rm(args[1]);
+                    break;
+                case "status":
+                    validateNumArgs(args, 1);
+                    Repository.status();
+                    break;
+                case "global-log":
+                    validateNumArgs(args, 1);
+                    Repository.globallog();
+                    break;
+                case "find":
+                    validateNumArgs(args, 2);
+                    Repository.find(args[1]);
+                    break;
+                case "checkout":
+                    Repository.checkout(args);
+                    break;
+                case "branch":
+                    validateNumArgs(args, 2);
+                    Repository.branch(args[1]);
+                    break;
+                case "rm-branch":
+                    validateNumArgs(args, 2);
+                    Repository.rmbranch(args[1]);
+                    break;
+                case "reset":
+                    validateNumArgs(args, 2);
+                    Repository.reset(args[1]);
+                    break;
+                case "merge":
+                    validateNumArgs(args, 2);
+                    Repository.merge(args[1]);
+                    break;
+                // TODO: FILL THE REST IN
+                default:  //如果改命令不是上面几个命令，输出....并exit；
+                    System.out.println("No command with that name exists.");
+                    System.exit(0);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
         }
     }
     public static void validateNumArgs(String[] args, int n) {//判断命令和参数个数是否匹配
